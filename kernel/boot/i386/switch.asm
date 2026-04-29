6c81653b369585bce3771ddf0a085d18dbc6f041
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-29 13:33:19 +0500

    Update switch.asm

--- kernel/boot/i386/switch.asm
+++ kernel/boot/i386/switch.asm
@@ -3,12 +3,12 @@
 switch:
     cli
     lgdt [gdt_descriptor]
-    
+
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
-    jmp 0x08:init
+    jmp CODE_SEG:init
 
 [bits 32]
 init:
@@ -18,8 +18,7 @@ init:
     mov ss, ax
     mov fs, ax
     mov gs, ax
-    sti
-
+    
     mov ebp, 0x80000
     mov esp, ebp
 
