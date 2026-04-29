a30355a7430d6d2be18b4eba2ea65d2ff8dce69c
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-29 21:39:56 +0500

    Update syscall.c

--- kernel/syscall/syscall.c
+++ kernel/syscall/syscall.c
@@ -1,34 +1,77 @@
 #include "../lib/syscall.h"
-#include "../lib/screen.h"
 #include "../lib/system.h"
 #include "../lib/stdbase.h"
+#include "../lib/types.h"
 
 static sys_process_t *current_process = {0};
 
-static int __attribute((noreturn)) sysExit(int retval) {
+int __attribute((noreturn)) sysExit(int retval) {
     taskExit((retval & 0xFF) << 8);
     for (;;) { ; }
 }
 
-static int sysGeteuid(void) {
+int sysGeteuid(void) {
     return current_process->user;
 }
 
-static int sysOpen(const char *file, int flags, int mode) {
+int sysOpen(const char *file, int flags, int mode) {
     int fd = 0;
+    
+    if (flags & O_CREAT) {
+        mode = ((mode_t *)&flags)[1];
+    }
+    
+    __asm__ volatile (
+        "int $0x80\n"
+        : "=a"(fd)
+        : "a"(5),
+          "b"(file),
+          "c"(flags),
+          "d"(mode)
+        : "memory"
+    );
+
     return fd;
 }
 
-static int sysRead(int fd, char *ptr, int len) {
+int sysCreate(const char *file, mode_t mode) {
+    int fd;
+    __asm__ volatile (
+        "int $0x80\n"
+        : "=a"(fd)
+        : "a"(8),
+          "b"(file),
+          "c"(mode)
+        : "memory"
+    );
+
     return fd;
 }
 
-static int sysWrite(int fd, char *ptr, int len) {
+int sysRead(int fd, char *ptr, int len) {
     return fd;
 }
 
-static int sysClose(int fd) {
+int sysWrite(int fd, char *ptr, int len) {
+    return fd;
+}
+
+int sysClose(int fd) {
     return 0;
+}
+
+off_t lseek(int fd, off_t offset, int whence) {
+    off_t res;
+    __asm__ volatile (
+        "int $0x80\n"
+        : "=a"(res)
+        : "a"(19),
+          "b"(fd),
+          "c"(offset),
+          "d"(whence)
+        : "memory"
+    );
+    return res;
 }
 
 static int (*syscalls[]) = {
@@ -38,6 +81,7 @@ static int (*syscalls[]) = {
     [SYS_READ] = sysRead,
     [SYS_WRITE] = sysWrite,
     [SYS_CLOSE] = sysClose,
+    [SYS_CREATE] = sysCreate
 };
 
 uint32_t num_syscalls = sizeof(syscalls) / sizeof(*syscalls);
