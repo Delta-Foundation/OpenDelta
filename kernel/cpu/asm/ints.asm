1ebe94006c8ee6c57d1727592ed0692c3a2d4ce5
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-01 16:32:06 +0500

    Update ints.asm

--- kernel/cpu/asm/ints.asm
+++ kernel/cpu/asm/ints.asm
@@ -23,3 +23,28 @@ isr%1:
     hmp isr_common_stub
 
 %endmacro
+
+global enable_ints
+global disable_ints
+global panic 
+global crash_me
+
+enable_ints:
+    sti
+    ret
+
+disable_ints:
+    cli
+    ret
+
+panic:
+    cli
+    hlt
+
+crash_me:
+    ; div by 0
+    ; mov ecx, 0x1337
+    ; mov eax, 0
+    ; div eax
+    int 0x80
+    ret
