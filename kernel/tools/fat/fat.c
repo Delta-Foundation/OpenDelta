e28da6100092ff31a31f1fe9b75194dad95a5f03
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-01 09:09:26 +0500

    Update and move utils/fat.c -> tools/fat.c

--- kernel/tools/fat/fat.c
+++ kernel/tools/fat/fat.c
@@ -4,3 +4,7 @@ boot_sect_t g_boot_sect;
 uint8_t* g_fat = NULL;
 dir_entry_t* g_root_dir = NULL;
 uint32_t g_root_dir_end;
+static fat_data_t g_actual_data;
+static fat_data_t g_data;
+static uint8_t *_g_fat = NULL;
+static uint32_t g_data_sect_lba;
