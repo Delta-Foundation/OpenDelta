7609552ec4425a51632f191db63aaf912ec61c6a
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-01 22:28:10 +0500

    Update midt.asm

--- kernel/boot/i386/midt.asm
+++ kernel/boot/i386/midt.asm
@@ -1,23 +1,26 @@
 section .data align=8
-    idt:
-        times 256 dq 0
-    idt_end:
 
-    idtr:
-        dw (idt_end - idt - 1)
-        dd idt
+idt:
+    times 256 dq 0
+idt_end:
+
+idtr:
+    dw (idt_end - idt - 1)
+    dd idt
+
 
 section .text
     global setup_idt
 
-    [bits 32]
+[bits 16]
 setup_idt:
     lidt [idtr]
     ret
 
-[bits 16]
 disable_timer:
+    pushf
     mov al, 0xFF
     out 0x21, al 
     out 0xA1, al
+    popf
     ret
