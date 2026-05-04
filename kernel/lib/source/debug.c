9ede7ca4823f76703c6620f470e6418b6650b797
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-04 19:22:14 +0500

    Create debug.c

---
+++ kernel/lib/source/debug.c
@@ -0,0 +1,31 @@
+#include "../debug.h"
+#include "../stdarg.h"
+#include "../stdbase.h"
+
+static const char* const log_colors[] = {
+    [LVL_DEBUG]        = "\033[2;37m",
+    [LVL_INFO]         = "\033[37m",
+    [LVL_WARN]         = "\033[1;33m",
+    [LVL_ERROR]        = "\033[1;31m",
+    [LVL_CRITICAL]     = "\033[1;37;41m",
+};
+
+static const char* const reset_color = "\033[0m";
+
+void logf(const char* module, debug_level level, const char* fmt, ...) 
+{
+    va_list args;
+    va_start(args, fmt);
+
+    if (level < MIN_LOG_LEVEL) {
+        return;
+    }
+
+    fputs(log_colors[level], (FILE *)debug);
+    fprintf((FILE *)debug, "[%s]", module);
+    vfprintf((FILE *)debug, fmt, args);
+    fputs(reset_color, (FILE *)debug);
+    fputc('\n', (FILE *)debug);
+
+    va_end(args);
+}
