5ede43d45143e1c072675f67cfd5f87fa62e1871
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-05 21:36:14 +0500

    Update disk.h

--- kernel/tools/fat/disk.h
+++ kernel/tools/fat/disk.h
@@ -2,13 +2,19 @@
 #define _TOOLS_DISK_H
 
 #include "../../lib/stdbase.h"
+#include "../../lib/types.h"
 
 #pragma once
 
 typedef struct {
     FILE * file;
+    uint8_t id;
+    uint16_t cylindres;
+    uint16_t sectors;
+    uint16_t heads;
 } DISK;
 
 boolean disk_init(DISK *disk, const char *filename);
+void disk_lba2chs(DISK* disk, uint32_t lba, uint16_t* cylinder_out, uint16_t* sector_out, uint16_t head_out);
 
 #endif 
