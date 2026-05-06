600506b7d46547eab5d325a0e4c586e4131f8b16
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-06 21:25:48 +0500

    Update tools.asm

--- kernel/tools/fat/asm/tools.asm
+++ kernel/tools/fat/asm/tools.asm
@@ -1,6 +1,6 @@
 %macro ENTER_REAL_MODE 0
     [bits 32]
-    jmp word 18h:.pmode16
+    jmp dw 0x18:.pmode16
 
 .pmode16:
     [bits 16]
@@ -8,7 +8,7 @@
     and al, ~1
     mov cr0, eax
 
-    jmp word 00h:.rmode
+    jmp dw 0x00:.rmode
 
 .rmode:
     mov ax, 0
@@ -27,9 +27,9 @@
     or al, 1
     mov cr0, eax
 
-    jmp dword 08h:.pmode
+    jmp dd 0x8:.pmode32
 
-.pmode:
+.pmode32:
     [bits 32]
     
     mov ax, 0x10
@@ -51,6 +51,9 @@
 global disk_get_drive_params
 global disk_reset
 global disk_read
+global get_next_block
+
+e820_sign equ 0x534D4150
 
 disk_get_drive_params:
     [bits 32]
@@ -72,7 +75,7 @@ disk_get_drive_params:
     mov di, 0           ; es:di - 0000:0000
     mov es, di
     stc
-    int 13h
+    int 0x13
 
     mov eax, 1
     sbb eax, 0
@@ -89,7 +92,7 @@ disk_get_drive_params:
     mov [es:si], bx
 
     xor ch, ch          ; sectors - lower 5 bits in cl
-    and cl, 3Fh
+    and cl, 0x3F
     
     LINEAR_TO_SEG_OFFSET [bp + 20], es, esi, si
     mov [es:si], cx
@@ -160,7 +163,7 @@ disk_read:
     shl cl, 6
     
     mov al, [bp + 16]    ; cl - sector to bits 0-5
-    and al, 3Fh
+    and al, 0x3F
     or cl, al
 
     mov dh, [bp + 20]   ; dh - head
@@ -169,14 +172,71 @@ disk_read:
 
     LINEAR_TO_SEG_OFFSET [bp + 28], es, ebx, bx
 
-    mov ah, 02h
+    mov ah, 0x02
     stc
-    int 13h
+    int 0x13
 
     mov eax, 1
     sbb eax, 0           ; 1 on success, 0 on fail   
 
     pop es
+    pop ebx
+
+    push eax
+
+    ENTER_PROTECTED_MODE
+
+    pop eax
+
+    mov esp, ebp
+    pop ebp
+    ret
+
+get_next_block:
+
+    push ebp             ; save old call frame
+    mov ebp, esp          ; initialize new call frame
+
+    ENTER_REAL_MODE
+
+    push ebx
+    push ecx
+    push edx
+    push esi
+    push edi
+    push ds
+    push es
+
+    LINEAR_TO_SEG_OFFSET [bp + 8], es, edi, di     ; es:di pointer to structure
+    
+    LINEAR_TO_SEG_OFFSET [bp + 12], ds, esi, si    ; ebx - pointer to continuationId
+    mov ebx, [ds:si]
+
+    mov eax, 0xE820                             ; eax - function
+    mov edx, e820_sign                          ; edx - signature
+    mov ecx, 24                                 ; ecx - size of structure
+
+    int 0x15
+
+    cmp eax, e820_sign
+    jne .Error
+
+    .IfSuccedeed:
+        mov eax, ecx            ; return size
+        mov [ds:si], ebx        ; fill continuation parameter
+        jmp .EndIf
+
+    .Error:
+        mov eax, -1
+
+    .EndIf:
+
+    pop es
+    pop ds
+    pop edi
+    pop esi
+    pop edx
+    pop ecx
     pop ebx
 
     push eax
