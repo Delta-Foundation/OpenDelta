286db3ab604f21e7824077f71269d58a07b40a51
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-06 19:16:43 +0500

    Update disk.c

--- kernel/tools/fat/disk.c
+++ kernel/tools/fat/disk.c
@@ -1,4 +1,5 @@
 #include "./disk.h"
+#include "./tools.h"
 
 boolean disk_init(DISK *disk, const char *filename) {
     disk->file = fopen((const char *)filename, "r");
@@ -14,3 +15,21 @@ void disk_lba2chs(DISK* disk, uint32_t lba,
     *cylinder_out = (lba / disk->sectors) / disk->heads;
     head_out = (lba / disk->sectors) % disk->heads;
 }
+
+boolean disk_read_sects(DISK* disk, uint32_t lba, uint8_t sectors, void* data_out) 
+{
+    uint16_t cylinder, sector, head;
+
+    disk_lba2chs(disk, lba, &cylinder, &sector, *&head);
+
+    for (int i = 0; i < 3; i++) {
+        if (disk_read(disk->id, cylinder, sector, head, sectors, data_out)) {
+            return (int)TRUE;
+        }
+
+        disk_reset(disk->id);
+    }
+
+    return (int)FALSE;
+}
+
