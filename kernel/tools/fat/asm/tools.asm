4cd29ea994e41938d9e11814ee080a750394732a
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-06 19:18:13 +0500

    Update tools.asm

--- kernel/tools/fat/asm/tools.asm
+++ kernel/tools/fat/asm/tools.asm
@@ -1,4 +1,4 @@
-%macro enter_real_mode 0
+%macro ENTER_REAL_MODE 0
     [bits 32]
     jmp word 18h:.pmode16
 
@@ -20,7 +20,7 @@
 %endmacro
 
 
-%macro enter_protected_mode 0
+%macro ENTER_PROTECTED_MODE 0
     cli
 
     mov eax, cr0
@@ -38,7 +38,7 @@
 
 %endmacro
 
-%macro linear_to_seg_offset 4
+%macro LINEAR_TO_SEG_OFFSET 4
 
     mov %3, %1      
     shr %3, 4
@@ -48,8 +48,74 @@
 
 %endmacro
 
+global disk_get_drive_params
 global disk_reset
 global disk_read
+
+disk_get_drive_params:
+    [bits 32]
+
+    push ebp             ; save old call frame
+    mov ebp, esp         ; initialize new call frame
+
+    ENTER_REAL_MODE
+
+    [bits 16]
+
+    push es
+    push bx
+    push esi
+    push di
+
+    mov dl, [bp + 8]    ; dl - disk drive
+    mov ah, 08h
+    mov di, 0           ; es:di - 0000:0000
+    mov es, di
+    stc
+    int 13h
+
+    mov eax, 1
+    sbb eax, 0
+
+    LINEAR_TO_SEG_OFFSET [bp + 12], es, esi, si
+    mov [es:si], bl
+
+    mov bl, ch          ; cylinders - lower bits in ch
+    mov bh, cl          ; cylinders - upper bits in cl (6-7)
+    shr bh, 6
+    inc bx
+
+    LINEAR_TO_SEG_OFFSET [bp + 16], es, esi, si
+    mov [es:si], bx
+
+    xor ch, ch          ; sectors - lower 5 bits in cl
+    and cl, 3Fh
+    
+    LINEAR_TO_SEG_OFFSET [bp + 20], es, esi, si
+    mov [es:si], cx
+
+    mov cl, dh          ; heads - dh
+    inc cx
+
+    LINEAR_TO_SEG_OFFSET [bp + 24], es, esi, si
+    mov [es:si], cx
+
+    pop di
+    pop esi
+    pop bx
+    pop es
+
+    push eax
+
+    ENTER_PROTECTED_MODE
+
+    [bits 32]
+
+    pop eax
+
+    mov esp, ebp
+    pop ebp
+    ret
 
 disk_reset:
     [bits 32]
@@ -57,7 +123,7 @@ disk_reset:
     push ebp
     mov ebp, esp
 
-    enter_real_mode
+    ENTER_REAL_MODE
 
     mov ah, 0
     mov dl, [bp + 8]
@@ -69,7 +135,7 @@ disk_reset:
 
     push eax 
 
-    enter_protected_mode
+    ENTER_PROTECTED_MODE
 
     pop eax
 
@@ -82,12 +148,11 @@ disk_read:
     push ebp             ; save old call frame
     mov ebp, esp          ; initialize new call frame
 
-    enter_protected_mode
+    ENTER_PROTECTED_MODE
 
     push ebx
     push es
 
-    ; setup args
     mov dl, [bp + 8]    ; dl - drive
 
     mov ch, [bp + 12]    ; ch - cylinder (lower 8 bits)
@@ -102,7 +167,7 @@ disk_read:
 
     mov al, [bp + 24]   ; al - count
 
-    linear_to_seg_offset [bp + 28], es, ebx, bx
+    LINEAR_TO_SEG_OFFSET [bp + 28], es, ebx, bx
 
     mov ah, 02h
     stc
@@ -116,7 +181,7 @@ disk_read:
 
     push eax
 
-    enter_protected_mode
+    ENTER_PROTECTED_MODE
 
     pop eax
 
