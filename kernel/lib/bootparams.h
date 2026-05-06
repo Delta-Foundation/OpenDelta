32d1cff9824cff22efc41c5314294ea6cbf20594
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-06 21:24:37 +0500

    Update bootparams.h

--- kernel/lib/bootparams.h
+++ kernel/lib/bootparams.h
@@ -9,16 +9,16 @@ typedef struct {
     uint64_t begin, length;
     uint32_t type;
     uint32_t ACPI;
-} MemoryRegion;
+} mem_region_t;
 
 typedef struct {
     int region_count;
-    MemoryRegion* regions;
-} MemoryInfo;
+    mem_region_t* regions;
+} mem_info_t;
 
 typedef struct {
-    MemoryInfo memory;
+    mem_info_t memory;
     uint8_t boot_device;
-} BootParams;
+} boot_params_t;
 
 #endif 
