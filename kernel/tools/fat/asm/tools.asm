1b46627b8cf38ab1ca2a3cbd486e18e96ee6a384
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-10 08:50:01 +0500

    Update tools.asm

--- kernel/tools/fat/asm/tools.asm
+++ kernel/tools/fat/asm/tools.asm
@@ -1,3 +1,11 @@
+%include "tools/fat/asm/macros.asm"
+
+e820_sign equ 0x534D4150
+
+section .bss
+    alignb 16
+    pmode_stack_backup resd 1
+
 section .text 
     [bits 32]
 
@@ -6,245 +14,214 @@ section .text
     global disk_read
     global get_next_block
 
-    e820_sign equ 0x534D4150
-
-%macro ENTER_REAL_MODE 0
-    [bits 32]
-    jmp 18h:%%pmode16
-
-%%pmode16:
-    [bits 16]
-    mov eax, cr0
-    and al, ~1
-    mov cr0, eax
-
-    jmp 00h:%%rmode
-
-%%rmode:
-    mov ax, 0
-    mov ds, ax
-    mov ss, ax
-
-    sti
-%endmacro
-
-%macro ENTER_PROTECTED_MODE 0
-    cli
-
-    mov eax, cr0
-    or al, 1
-    mov cr0, eax
-
-    jmp 08h:%%pmode32
-
-%%pmode32:
-    [bits 32]
-
-    mov ax, 0x10
-    mov ds, ax
-    mov ss, ax
-%endmacro
-
-%macro LINEAR_TO_SEG_OFFSET 4
-
-    mov %3, %1      
-    shr %3, 4
-    mov %2, %4
-    mov %3, %1 
-    and %3, 0xf
-
-%endmacro
-
 disk_get_drive_params:
-    [bits 32]
-
     push ebp             ; save old call frame
     mov ebp, esp         ; initialize new call frame
+    push eax 
+    push ebx 
+    push ecx 
+    push edx
+    push esi 
+    push edi
+    push ds 
+    push es 
 
+    mov [pmode_stack_backup], esp 
     ENTER_REAL_MODE
+    jmp 0x00:rmode_disk_get_drive_params
 
+pmode_disk_get_drive_params:
+    ENTER_PROTECTED_MODE
+    mov esp, [pmode_stack_backup]
+
+    pop es 
+    pop ds 
+    pop edi 
+    pop esi 
+    pop edx 
+    pop ecx 
+    pop ebx 
+    pop eax 
+    mov esp, ebp 
+    pop ebp 
+    ret 
+
+disk_reset:
+    push ebp
+    mov ebp, esp 
+    push eax 
+    push edx
+    push ds 
+    push es 
+
+    mov [pmode_stack_backup], esp 
+    ENTER_REAL_MODE
+    jmp 0x00:rmode_disk_reset
+
+pmode_disk_reset:
+    ENTER_PROTECTED_MODE
+    mov esp, [pmode_stack_backup]
+
+    pop es 
+    pop ds 
+    pop edx 
+    pop eax 
+    mov esp, ebp
+    pop ebx 
+    ret 
+
+disk_read:
+    push ebp 
+    mov ebp, esp 
+    push eax 
+    push ebx 
+    push ecx 
+    push edx 
+    push esi 
+    push edi 
+    push ds 
+    push es 
+
+    mov [pmode_stack_backup], esp 
+    ENTER_REAL_MODE
+    jmp 0x00:rmode_disk_read
+
+pmode_disk_read:
+    ENTER_PROTECTED_MODE
+    mov esp, [pmode_stack_backup]
+
+    pop es 
+    pop ds 
+    pop edi 
+    pop esi 
+    pop edx 
+    pop ecx 
+    pop ebx 
+    pop eax 
+    mov esp, ebp 
+    pop ebp 
+    ret 
+
+get_next_block:
+    push ebp 
+    mov ebp, esp 
+    push eax 
+    push ebx 
+    push ecx 
+    push edx 
+    push esi 
+    push edi 
+    push ds  
+    push es 
+
+    mov [pmode_stack_backup], esp
+    ENTER_REAL_MODE 
+    jmp 0x00:rmode_get_next_block
+
+pmode_get_next_block:
+    ENTER_PROTECTED_MODE
+    mov esp, [pmode_stack_backup]
+
+    pop es 
+    pop ds 
+    pop edi 
+    pop esi 
+    pop edx 
+    pop ecx 
+    pop ebx 
+    pop eax 
+    mov esp, ebp 
+    pop ebp 
+    ret 
+
+section .text.real 
     [bits 16]
 
-    push es
-    push bx
-    push esi
-    push di
-
-    mov dl, [bp + 8]    ; dl - disk drive
+rmode_disk_get_drive_params:
+    mov dl, [bp + 8]
     mov ah, 08h
-    mov di, 0           ; es:di - 0000:0000
-    mov es, di
-    stc
+    xor di, di 
+    mov es, di 
+    stc 
     int 13h 
-
     mov eax, 1
     sbb eax, 0
 
-    LINEAR_TO_SEG_OFFSET [bp + 12], es, esi, si
-    mov [es:si], bl
+    LINEAR_TO_SEG_OFFSET [bp + 12], es, esi, si 
+    mov [es:si], bl 
 
-    mov bl, ch          ; cylinders - lower bits in ch
-    mov bh, cl          ; cylinders - upper bits in cl (6-7)
-    shr bh, 6
-    inc bx
+    mov bl, ch
+    mov bh, cl 
+    shr bh, 6 
+    inc bx 
+    LINEAR_TO_SEG_OFFSET [bp + 16], es, esi, si 
+    mov [es:si], bx 
 
-    LINEAR_TO_SEG_OFFSET [bp + 16], es, esi, si
-    mov [es:si], bx
-
-    xor ch, ch          ; sectors - lower 5 bits in cl
-    and cl, 3Fh
-    
-    LINEAR_TO_SEG_OFFSET [bp + 20], es, esi, si
+    xor ch, ch 
+    and cl, 3Fh 
+    LINEAR_TO_SEG_OFFSET [bp + 20], es, esi, si 
     mov [es:si], cx
 
-    mov cl, dh          ; heads - dh
-    inc cx
+    mov cl, dh 
+    inc cx 
+    LINEAR_TO_SEG_OFFSET [bp + 24], es, esi, si 
+    mov [es:si], cx 
 
-    LINEAR_TO_SEG_OFFSET [bp + 24], es, esi, si
-    mov [es:si], cx
+    push eax 
+    jmp pmode_disk_get_drive_params  
 
-    pop di
-    pop esi
-    pop bx
-    pop es
-
-    push eax
-
-    ENTER_PROTECTED_MODE
-
-    [bits 32]
-
-    pop eax
-
-    mov esp, ebp
-    pop ebp
-    ret
-
-disk_reset:
-    [bits 32]
-
-    push ebp
-    mov ebp, esp
-
-    ENTER_REAL_MODE
-
-    mov ah, 0
+rmode_disk_reset:
     mov dl, [bp + 8]
+    mov ah, 00h
+    stc 
+    int 13h 
+    mov eax, 1 
+    sbb eax, 0
+    push eax 
+    jmp pmode_disk_reset
+
+rmode_disk_read:
+    mov dl, [bp + 8]
+    mov ch, [bp + 12]
+    mov cl, [bp + 13]
+    shl cl, 6 
+    mov al, [bp + 16]
+    and al, 3Fh 
+    or cl, al 
+    mov dh, [bp + 20]
+    mov al, [bp + 24]
+
+    LINEAR_TO_SEG_OFFSET [bp + 28], es, ebx, bx 
+    mov ah, 02h 
     stc 
     int 13h 
 
-    mov eax, 1
+    mov eax, 1 
     sbb eax, 0
-
     push eax 
+    jmp pmode_disk_read
 
-    ENTER_PROTECTED_MODE
-
-    pop eax
-
-    mov esp, ebp
-    pop ebp
-    ret
-
-disk_read:
-    
-    push ebp             ; save old call frame
-    mov ebp, esp          ; initialize new call frame
-
-    ENTER_PROTECTED_MODE
-
-    push ebx
-    push es
-
-    mov dl, [bp + 8]    ; dl - drive
-
-    mov ch, [bp + 12]    ; ch - cylinder (lower 8 bits)
-    mov cl, [bp + 13]    ; cl - cylinder to bits 6-7
-    shl cl, 6
-    
-    mov al, [bp + 16]    ; cl - sector to bits 0-5
-    and al, 3Fh
-    or cl, al
-
-    mov dh, [bp + 20]   ; dh - head
-
-    mov al, [bp + 24]   ; al - count
-
-    LINEAR_TO_SEG_OFFSET [bp + 28], es, ebx, bx
-
-    mov ah, 02h 
-    stc
-    int 13h 
-
-    mov eax, 1
-    sbb eax, 0           ; 1 on success, 0 on fail   
-
-    pop es
-    pop ebx
-
-    push eax
-
-    ENTER_PROTECTED_MODE
-
-    pop eax
-
-    mov esp, ebp
-    pop ebp
-    ret
-
-get_next_block:
-
-    push ebp             ; save old call frame
-    mov ebp, esp          ; initialize new call frame
-
-    ENTER_REAL_MODE
-
-    push ebx
-    push ecx
-    push edx
-    push esi
-    push edi
-    push ds
-    push es
-
-    LINEAR_TO_SEG_OFFSET [bp + 8], es, edi, di     ; es:di pointer to structure
-    
-    LINEAR_TO_SEG_OFFSET [bp + 12], ds, esi, si    ; ebx - pointer to continuationId
+rmode_get_next_block:
+    LINEAR_TO_SEG_OFFSET [bp + 8], es, edi, di
+    LINEAR_TO_SEG_OFFSET [bp + 12], ds, esi, si
     mov ebx, [ds:si]
 
-    mov eax, 0xE820                             ; eax - function
-    mov edx, e820_sign                          ; edx - signature
-    mov ecx, 24                                 ; ecx - size of structure
-
-    int 0x15
+    mov eax, 0xE820
+    mov edx, e820_sign
+    mov ecx, 24
+    int 15h
 
     cmp eax, e820_sign
-    jne .Error
+    jne .e820_fail
 
-    .IfSuccedeed:
-        mov eax, ecx            ; return size
-        mov [ds:si], ebx        ; fill continuation parameter
-        jmp .EndIf
+.e820_ok:
+    mov eax, ecx 
+    mov [ds:si], ebx
+    jmp .e820_end
 
-    .Error:
-        mov eax, -1
+.e820_fail:
+    mov eax, -1
 
-    .EndIf:
-
-    pop es
-    pop ds
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
-
+.e820_end:
     push eax
-
-    ENTER_PROTECTED_MODE
-
-    pop eax
-
-    mov esp, ebp
-    pop ebp
-    ret
+    jmp pmode_get_next_block
