d9e740ed3e09729948d863b61828dfaf2558c623
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-14 15:56:57 +0500

    Update string.h

--- kernel/lib/string.h
+++ kernel/lib/string.h
@@ -3,9 +3,11 @@
 
 #include "types.h"
 
-#define size_t	unsigned long int
+typedef uint32_t _wchar_t;
 
 int strindex(char s[], char t[]);
+
+const char* strchar(const char* str, char ch);
 
 int strcmp(const char *stra, const char *strb);
 int strncmp(const char *cs, const char *ct, unsigned int n); 
@@ -22,5 +24,8 @@ void int_to_ascii(int n, char str[]);
 void hex_to_ascii(int n, char str[]);
 
 int sprintf(char *buf, const char *fmt);
+
+_wchar_t* utf16_to_codepoint(_wchar_t* string, int* codepoint);
+char* codepoint_to_utf8(int codepoint, char* string_out);
 
 #endif
