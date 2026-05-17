0d674decb414fa27433724564c91b2fec63b0f1d
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-17 11:57:21 +0500

    Update types.c
    added transformations to int* in conditions on TRUE

--- kernel/lib/source/types.c
+++ kernel/lib/source/types.c
@@ -1,16 +1,16 @@
 /* without stdio.h and other std libs */
 #include "../types.h"
 
-size_t size_add(size_t a, size_t b, int* overflow) {
+size_t sizeAdd(size_t a, size_t b, int* overflow) {
     size_t res = a + b;
-    if (overflow == TRUE) {
+    if (overflow == (int *)TRUE) {
         *overflow = (res < a) ? 1 : 0;
     }
 
     return res;
 }
 
-size_t size_mul(size_t a, size_t b, int* overf) {
+size_t sizeMul(size_t a, size_t b, int* overf) {
     if (a == 0 || b == 0) {
         if (overf) {
             *overf = 0;
@@ -20,22 +20,22 @@ size_t size_mul(size_t a, size_t b, int* overf) {
 
     size_t res = a * b;
 
-    if (overf == TRUE) {
+    if (overf == (int *)TRUE) {
         *overf = (res / a != b) ? 1 : 0;
     }
 
     return res;
 }
 
-size_t size_sub(size_t a, size_t b, int* underf) {
-    if (underf == TRUE) {
+size_t sizeSub(size_t a, size_t b, int* underf) {
+    if (underf == (int *)TRUE) {
         *underf = (b > a) ? 1 : 0;
     }
 
     return (b > a) ? 0 : (a - b);
 }
 
-int size_cmp(size_t a, size_t b) {
+int sizeCmp(size_t a, size_t b) {
     if (a < b) {
         return -1;
     }
@@ -68,14 +68,4 @@ long atol(const char *s)
     }
 
     return n;
-}
-
-int lower(int c)
-{
-    if (c >= 'A' && c <= 'Z') {
-        return c + 'a' - 'A';
-    } 
-    else {
-        return c;
-    }
 }
