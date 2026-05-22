3a31098e48cfa3a9f7d69b8186a749003bc04180
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-22 16:02:43 +0500

    Update clean.sh

--- scripts/kern/clean.sh
+++ scripts/kern/clean.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 cd ~/OpenDelta/kernel/
+
 function clean {
     echo "clean .obj binaries"
     rm -f obj/kernel.o obj/stdbase.o obj/idt.o obj/mem.o 
@@ -10,13 +11,14 @@ function clean {
         \ obj/shm.o obj/fs.o obj/list.o obj/pipe.o
         \ obj/idta.o obj/pic.o obj/fpu.o obj/sys.o
         \ obj/proc.o obj/task.o obj/isr.o obj/tools.o 
-        \ obj/fat.o obj/disk.o obj/mbr.o
+        \ obj/fat.o obj/disk.o obj/mbr.o obj/elf.o 
 
     echo "clean kernel.map"
     rm -f kernel.map
 
     echo "clean .bin and .img binaries"
-    rm -f img/kernel.bin img/boot.bin img/open-delta.img
+    rm -f img/kernel.bin img/kernel.elf 
+        \ img/boot.bin img/open-delta.img
 }
 
 clean
