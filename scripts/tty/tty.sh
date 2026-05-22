afe1e3a502b2afd7620dd8dbb837ab239f36fa6e
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-22 15:09:59 +0500

    Update tty.sh

--- scripts/tty/tty.sh
+++ scripts/tty/tty.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# путь к устройству tty
-TTY_PATH="/dev/tty1"
+function tty() {
+    # путь к устройству tty
+    TTY_PATH="/dev/tty1"
 
-# скорость baudrate
-BAUD_RATE=9600
+    # скорость baudrate
+    BAUD_RATE=9600
 
-# проверяем на начилие даннного устройства по путю $TTY_PATH
-if [ ! -e "$TTY_PATH" ]; then
-    echo "[err]: устройство $TTY_PATH не найдено."
-    exit 1
-fi
+    # проверяем на начилие даннного устройства по путю $TTY_PATH
+    if [ ! -e "$TTY_PATH" ]; then
+        echo "[err]: устройство $TTY_PATH не найдено."
+        exit 1
+    fi
 
-# устанавливаем agetty на устройстве
-agetty -L $BAUD_RATE $TTY_PATH
+    # устанавливаем agetty на устройстве
+    agetty -L $BAUD_RATE $TTY_PATH
+}
+
+tty
