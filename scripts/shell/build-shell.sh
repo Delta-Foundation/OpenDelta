292ed351e201f45c445c1e751557c39c95f5ae93
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-22 16:03:08 +0500

    Update build-shell.sh

--- scripts/shell/build-shell.sh
+++ scripts/shell/build-shell.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-function build() {
-    cd ~/OpenDelta/shell/
+function build {
+    cd ~/open-delta/kernel/shell/
     clang src/term.c src/files.c src/dltsh.c src/commands.c src/simple_comms.c -o bin/dltsh
     clang++ src/help_table.cpp -o bin/table -lncursesw
 	rustc src/dexide.rs -o bin/dexide
     rustc src/calc.rs -o bin/calc
-    rustc src/clocks.rs -o bin/clock
+    rustc src/clocks.rs -o bin/clocks
 }
 
-function run() { ./bin/term }
+function run {
+    ./bin/term
+}
+
+function clean {
+    echo "removing binaries"
+    rm -f bin/dltsh 
+        \ bin/dexide bin/calc 
+        \ bin/clocks bin/table
+}
 
 build && run
