93e878295bd085e17c0a37e792f0d5bdb55e45ca
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-23 14:26:24 +0500

    Update screen.h

--- kernel/lib/screen.h
+++ kernel/lib/screen.h
@@ -1,12 +1,12 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-int print_char(char c, int col, int row, unsigned int color);
+int printChar(char c, int col, int row, unsigned int color);
 void kprint_at(const char *message, int col, int row, unsigned int color);
 void kprint(const char *message, unsigned int color);
 void kprintnl(void);
-void kprint_backspace(void);
-void clear_screen(void);
+void kprintb(void);
+void clearScreen(void);
 void delay(void);
 
 #endif
