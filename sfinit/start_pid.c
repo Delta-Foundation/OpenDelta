e1c62cc227964a85ffe8714ed663586fc7583a89
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-24 21:55:15 +0500

    Update start_pid.c

--- sfinit/start_pid.c
+++ sfinit/start_pid.c
@@ -3,15 +3,18 @@
  * стандартной библиотеки, если это не понадобится 
  * то поменяем на дефолтные библиотеки типо #include <stdio.h>
  */
-#include "../kernel/lib/system.h"
-#include "../kernel/lib/stdbase.h"
-#include "../kernel/lib/types.h"
+#include "../src/lib/system.h"
+#include "../src/lib/stdbase.h"
+#include "../src/lib/types.h"
 
-void start_pid1(void) 
-{
+void start_pid1(void) {
     pid_t pid = 1;
     prints("[SFINIT]: [initializing PID1]\n", CYAN);
-    spawnInit();
-    initProcTree();
-    initProc(pid);
+    initProc("SFINIT", pid);
+}
+
+void start_min_dltsh(void) {
+    pid_t dltsh_pid = 2;
+    prints("[SFINIT]: [initializing dltsh, pid: %d]\n", WHITE, dltsh_pid);
+    initProc("Minimal dltsh(lang)", dltsh_pid);
 }
