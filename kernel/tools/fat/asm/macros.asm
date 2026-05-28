bb0da6f989b663418626fa454297d1fff077a0e0
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-28 12:14:14 +0500

    Update macros.asm

--- kernel/tools/fat/asm/macros.asm
+++ kernel/tools/fat/asm/macros.asm
@@ -1,25 +1,28 @@
 %macro LINEAR_TO_SEG_OFFSET 4
-    mov %3, %1
+    mov %3, %1          ; загрузить 32-битный адрес
+    shr %3, 4           ; вычислить сегмент (linear >> 4)
+    push %3
+    pop %4              ; безопасно взять младшие 16 бит (сегмент)
+    mov %2, %4          ; записать в сегментный регистр (разрешено!)
+
+    mov %3, %1          ; перезагрузить исходный адрес
+    and %3, 0xF         ; вычислить смещение (linear & 0xF)
     push %3
     pop %4
-    shr %3, 4
-    push %3
-    pop %2
 %endmacro
 
 %macro ENTER_PROTECTED_MODE 0
-    [bits 16]
     cli
-    mov eax, cr0 
-    or al, 1
+    mov eax, cr0
+    or al, 0x01
     mov cr0, eax
     jmp 0x08:%%pmode_start
 %%pmode_start:
     [bits 32]
     mov ax, 10
-    mov ds, ax 
+    mov ds, ax
     mov es, ax
-    mov fs, ax 
-    mov gs, ax  
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
 %endmacro
