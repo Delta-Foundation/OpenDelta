38e9391c1819246dd1ad35c4ba85d82875cc5f84
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-30 21:15:47 +0500

    Update tools.h

--- kernel/tools/fat/headers/tools.h
+++ kernel/tools/fat/headers/tools.h
@@ -5,8 +5,6 @@
 
 #include "../../../lib/types.h"
 
-#define ASMCALL __attribute__((cdecl))
-
 typedef struct {
     uint64_t base;
     uint64_t len;
@@ -22,20 +20,20 @@ enum E820_MEM_BLOCK_TYPE {
     E820_BAD_MEM = 5,
 }; 
 
-void ASMCALL disk_get_drive_params(uint8_t drive,
+void disk_get_drive_params(uint8_t drive,
         uint8_t* drive_type_out,
         uint16_t* cylindres_out,
         uint16_t* sectors_out,
         uint16_t* heads_out);
 
-boolean ASMCALL disk_reset(uint8_t drive);
-boolean ASMCALL disk_read(uint8_t drive,
+boolean disk_reset(uint8_t drive);
+boolean disk_read(uint8_t drive,
         uint16_t cylinder,
         uint16_t sector,
         uint16_t head,
         uint8_t count,
         void* lower_data_out);
 
-int ASMCALL get_next_block(e820_mem_block* block, uint32_t* continuation_id);
+int get_next_block(e820_mem_block* block, uint32_t* continuation_id);
 
 #endif 
