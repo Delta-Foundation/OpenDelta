c68d3dbf5d49ad6ccf3bd4a395366ce910ee671c
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-30 23:07:14 +0500

    Update install_deps.sh

--- scripts/base/install_deps.sh
+++ scripts/base/install_deps.sh
@@ -2,23 +2,23 @@
 
 # !NO DEBIAN AND APT
 
-function installDeps {
-    clear
+clear
 
-    while true; do
+function kernel_deps {
+    while true; do 
         echo "choose your distro group"
         echo "1.arch, 2.void"
         read group
 
         if [ "$group" == "1" ]; then
             sudo pacman -S clang llvm nasm make
-            sudo pacman -S qemu-system-i386 qemu-system-x86_64
+            sudo pacman -S qemu-system-i386 qemu-common qemu-firmware 
             sudo pacman -S lld git
             break
         elif [ "$group" == "2" ]; then
             sudo xbps-install -S clang llvm nasm make
-            sudo xbps-install -S git ld.lld
-            sudo xbps-install -S qemu-system-i386
+            sudo xbps-install -S git lld
+            sudo xbps-install -S qemu-system-i386 qemu-common qemu-firmware
             break
         else
             echo "choose only 1 or 2"
@@ -26,4 +26,28 @@ function installDeps {
     done
 }
 
-installDeps
+function shell_deps {
+    while true; do 
+        echo "choose your distro group"
+        echo "1.arch, 2.void"
+        read group 
+
+        if [ "$group" == "1" ]; then
+            sudo pacman -S rust cargo
+            sudo pacman -S ncurses ncurses-devel ncurses-libs
+        elif [ "$group" == "2" ]; then
+            sudo xbps-install -S rust cargo
+            sudo xbps-install -S ncurses ncurses-devel ncurses-libs
+        else
+            echo "choose only 1 or 2"
+        fi 
+    done
+}
+
+function main {
+    echo "install kernel dependencies"
+    kernel_deps
+
+    echo "install shell dependencies"
+    shell_deps
+}
