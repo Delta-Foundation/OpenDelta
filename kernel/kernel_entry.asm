4f1bdb7f715f327f753eb7315ea100faca591546
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-31 22:26:50 +0500

    Update kernel_entry.asm

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -9,15 +9,25 @@ global load_idt
     ; C functions
 extern kmain
 
-section .text.start
+section .data
+    nl db 'W', 0x0A
+
+section .bss
+    align 16
+    stack_bot: resb 0x4000
+    stack_top:
+
+section .text
 
 _start:
     cli
+    cld
 
     mov dword [0xB8000], 0x0F4B0F4B
 
     mov esp, stack_top
-
+    push ebx 
+    push eax 
     call kmain 
 
     cli
@@ -25,11 +35,6 @@ _start:
 .hang:
     hlt
     jmp .hang
-
-_exit:
-    mov ebx, 0
-    mov eax, 1
-    int 0x80
 
 readp:
     push ebp
@@ -56,17 +61,9 @@ load_idt:
     push ebp
 
     mov ebp, esp
-    lidt [ebp + 8]
+    mov eax, [ebp + 8]
+    lidt [eax]
 
     sti
     pop ebp
     ret
-
-section .data
-    nl db 'W', 0x0A
-
-section .bss
-    align 16
-    stack_bot:
-        resb 0x4000
-    stack_top:
