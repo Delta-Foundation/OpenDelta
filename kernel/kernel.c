3b8cdbef6c864f3f01efffeab15de527349ef9e9
deltadev-rsc <deltaqxq174@mail.ru>
2026-05-31 22:27:05 +0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -17,20 +17,24 @@
 #include "./lib/bootparams.h"
 #include "./tty/header/min_dltsh.h"
 
+#define DLTKERNEL_MAGIC 0xDEADB007u
+
 extern char readp(uint16_t port);
 extern void writep(uint16_t port, uint8_t data);
 extern void load_idt(uintptr_t *idt_ptr);
 
 IDTEntry IDT[IDT_SIZE];
-struct multiboot *mboot = NULL;
-boot_params_t *boot_drive;
-boot_params_t *boot_params;
-void* partition;
+uint8_t *boot_drive = (uint8_t *)0x80;
+boot_params_t *boot_params = NULL;
+void* partition = NULL;
 
 static void kpanic(const char *panic_msg, ...) {
     prints("KERNEL PANIC: ", WHITE);
     prints(panic_msg, WHITE);
     prints("\n", WHITE);
+
+    __asm__ volatile ( "cli" );
+
     for (;;) { 
         __asm__ volatile ( "hlt" );
     }
@@ -39,18 +43,29 @@ static void kpanic(const char *panic_msg, ...) {
 __attribute__((noreturn)) void __stack_chk_fail(void) {
     while (1) {
         kpanic("[KERNEL PANIC]: [?????????? IDI NAHUY!]");
-        __asm__ volatile ("hlt");
+        __asm__ volatile ( "cli" );
+        __asm__ volatile ( "hlt" );
     }
 }
 
-void kmain(void)
+void kmain(uint32_t magic, boot_params_t* bp)
 {
+    if (magic != DLTKERNEL_MAGIC) {
+        kpanic("[KERNEL PANIC]: [bad bootloader magic]\n", RED);
+    }
+
+    if (!bp) {
+        kpanic("[KERNEL PANIC]: [boot parameters pointer is NULL]\n", RED);
+    }
+
+    boot_params = bp;
+    boot_drive = (uint8_t *)(bp->boor_drive & 0xFF);
+
+    clearScreen();
 
     const char *welcome = "Welcome to the OpenDelta!\n";
     const char *os_name = "OS: OpenDelta v0.1-a\n";
     const char *kern_name = "Kernel: dltkernel v0.0.9-p\n";    
-
-    clearScreen();
 
     prints(welcome, CYAN);
     prints(os_name, WHITE);
@@ -59,23 +74,14 @@ void kmain(void)
     prints("[info]: [initializing i386 GDT]\n", WHITE);
     i386_GDT_init();
 
-    delay();
-
     prints("[info]: [initializing isr and irq]\n", WHITE);
     isr_install();
     irq_init();
 
-    __asm__ volatile ( "int $3" );
-
-    delay();
 
     prints("[info]: [install memory management and shared memory]\n", WHITE);
-
-    mboot_ptr = mboot;
-    paggingInstall(mboot_ptr->mem_upper + mboot_ptr->mem_lower);
+    paggingInstall(bp->mem_lower + bp->mem_upper);
     heapInstall();
-    delay();
-
     shmInstall();
     
     prints("[info]: [install hardware drivers]\n", WHITE);
@@ -110,12 +116,8 @@ void kmain(void)
     prints("[info]: [starting minimal dltsh]", WHITE);
     min_dltsh();
 
-    delay();
-
-// end:
+end:
     while (TRUE) {
         __asm__ __volatile__ ("hlt");
     }
-
-    kpanic("[KERNEL PANIC]: [end of kernel main function]", CYAN);
 }
