3803e776f7555e335850a0de6e3f820984cfa8a7
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-02 08:08:27 +0500

    Update stdbase.c

--- kernel/lib/source/stdbase.c
+++ kernel/lib/source/stdbase.c
@@ -536,6 +536,22 @@ int getline(char *line, int max)
     }
 }
 
+int mode_getline(char s[], int lim) {
+    int c, i;
+
+    i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n') {
+        s[i++] = c;
+    }
+
+    if (c == '\n') {
+        s[i++] = c;
+    }
+
+    s[i] = '\0';
+    return i;
+}
+
 FILE *fopen(char *name, char *mode) 
 {
     signed int fd = 0;
