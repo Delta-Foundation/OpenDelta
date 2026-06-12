a44b6c9e5c463b6782a98029127b28cc0ab9283d
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-12 21:11:05 +0500

    Update isr.h

--- kernel/lib/isr.h
+++ kernel/lib/isr.h
@@ -4,55 +4,55 @@
 #include "types.h"
 
 // ISRs reserved for CPU exceptions
-extern void isr0(void);
-extern void isr1(void);
-extern void isr2(void);
-extern void isr3(void);
-extern void isr4(void);
-extern void isr5(void);
-extern void isr6(void);
-extern void isr7(void);
-extern void isr8(void);
-extern void isr9(void);
-extern void isr10(void);
-extern void isr11(void);
-extern void isr12(void);
-extern void isr13(void);
-extern void isr14(void);
-extern void isr15(void);
-extern void isr16(void);
-extern void isr17(void);
-extern void isr18(void);
-extern void isr19(void);
-extern void isr20(void);
-extern void isr21(void);
-extern void isr22(void);
-extern void isr23(void);
-extern void isr24(void);
-extern void isr25(void);
-extern void isr26(void);
-extern void isr27(void);
-extern void isr28(void);
-extern void isr29(void);
-extern void isr30(void);
-extern void isr31(void);
+extern void __attribute__((cdecl)) isr0(void);
+extern void __attribute__((cdecl)) isr1(void);
+extern void __attribute__((cdecl)) isr2(void);
+extern void __attribute__((cdecl)) isr3(void);
+extern void __attribute__((cdecl)) isr4(void);
+extern void __attribute__((cdecl)) isr5(void);
+extern void __attribute__((cdecl)) isr6(void);
+extern void __attribute__((cdecl)) isr7(void);
+extern void __attribute__((cdecl)) isr8(void);
+extern void __attribute__((cdecl)) isr9(void);
+extern void __attribute__((cdecl)) isr10(void);
+extern void __attribute__((cdecl)) isr11(void);
+extern void __attribute__((cdecl)) isr12(void);
+extern void __attribute__((cdecl)) isr13(void);
+extern void __attribute__((cdecl)) isr14(void);
+extern void __attribute__((cdecl)) isr15(void);
+extern void __attribute__((cdecl)) isr16(void);
+extern void __attribute__((cdecl)) isr17(void);
+extern void __attribute__((cdecl)) isr18(void);
+extern void __attribute__((cdecl)) isr19(void);
+extern void __attribute__((cdecl)) isr20(void);
+extern void __attribute__((cdecl)) isr21(void);
+extern void __attribute__((cdecl)) isr22(void);
+extern void __attribute__((cdecl)) isr23(void);
+extern void __attribute__((cdecl)) isr24(void);
+extern void __attribute__((cdecl)) isr25(void);
+extern void __attribute__((cdecl)) isr26(void);
+extern void __attribute__((cdecl)) isr27(void);
+extern void __attribute__((cdecl)) isr28(void);
+extern void __attribute__((cdecl)) isr29(void);
+extern void __attribute__((cdecl)) isr30(void);
+extern void __attribute__((cdecl)) isr31(void);
 // IRQ Definitions
-extern void irq0(void);
-extern void irq1(void);
-extern void irq2(void);
-extern void irq3(void);
-extern void irq4(void);
-extern void irq5(void);
-extern void irq6(void);
-extern void irq7(void);
-extern void irq8(void);
-extern void irq9(void);
-extern void irq10(void);
-extern void irq11(void);
-extern void irq12(void);
-extern void irq13(void);
-extern void irq14(void);
-extern void irq15(void);
+extern void __attribute__((cdecl)) irq0(void);
+extern void __attribute__((cdecl)) irq1(void);
+extern void __attribute__((cdecl)) irq2(void);
+extern void __attribute__((cdecl)) irq3(void);
+extern void __attribute__((cdecl)) irq4(void);
+extern void __attribute__((cdecl)) irq5(void);
+extern void __attribute__((cdecl)) irq6(void);
+extern void __attribute__((cdecl)) irq7(void);
+extern void __attribute__((cdecl)) irq8(void);
+extern void __attribute__((cdecl)) irq9(void);
+extern void __attribute__((cdecl)) irq10(void);
+extern void __attribute__((cdecl)) irq11(void);
+extern void __attribute__((cdecl)) irq12(void);
+extern void __attribute__((cdecl)) irq13(void);
+extern void __attribute__((cdecl)) irq14(void);
+extern void __attribute__((cdecl)) irq15(void);
 
 #define IRQ0	32
 #define IRQ1	33
@@ -76,19 +76,19 @@ extern void irq15(void);
 
 typedef struct {
 	uint32_t ds;
-	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	uint32_t edi, esi, ebp, useless, ebx, edx, ecx, eax;
 	uint32_t int_no, err_code;
-	uint32_t eip, cs, eflags, useresp, ss;
+	uint32_t eip, cs, eflags, esp, ss;
 } __attribute__((packed)) regs_t;
 
-typedef void (*isr_t)(regs_t);
+typedef void (*isr_t)(regs_t*);
 
 uint8_t enable_ints(void);
 uint8_t disable_ints(void);
 void panic(void);
 void isr_install(void);
-void isr_handler(regs_t r);
-void irq_handler(regs_t r);
+void isr_handler(regs_t *r);
+void irq_handler(regs_t *r);
 void irq_install(void);
 void irq_regs_handler(int irq, isr_t handler);
 
