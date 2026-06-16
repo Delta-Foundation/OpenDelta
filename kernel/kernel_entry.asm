2b865fc23c74312436f7c60138d75083c5877638
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-16 23:18:47 +0500

    Update kernel_entry.asm

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -6,45 +6,46 @@ global readp  ; read port
 global writep ; write port
 [extern __bss_start]
 [extern __bss_end]
+[extern stack_top]
 
     ; C functions
 [extern kmain]
 
 VIDEO_MEMORY    equ 0xB8000
-KERNEL_CS       equ 0x08 
-KERNEL_DS       equ 0x10 
+KERNEL_CS       equ 0x08
+KERNEL_DS       equ 0x10
 
-section .text
+section .text._start
 
 _start:
     cli
 
     mov ax, KERNEL_DS
-    mov ds, ax 
-    mov es, ax 
-    mov fs, ax 
-    mov gs, ax 
-    mov ss, ax 
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
     mov esp, stack_top
 
     ; Marker 1: segments + stack ready. Kernel started
-    mov dword [VIDEO_MEMORY], 0x2F312F31 
-    
-    mov edi, __bss_start 
-    mov ecx, __bss_end 
-    sub ecx, edi 
-    xor al, al 
-    cld 
+    mov dword [VIDEO_MEMORY], 0x2F312F31
+
+    mov edi, __bss_start
+    mov ecx, __bss_end
+    sub ecx, edi
+    xor al, al
+    cld
     rep stosb
 
-    ; Marker 2: BSS cleared 
-    mov dword [VIDEO_MEMORY + 4], 0x2F322F32 
+    ; Marker 2: BSS cleared
+    mov dword [VIDEO_MEMORY + 4], 0x2F322F32
 
     ; Marker 3: call kmain
-    mov dword [VIDEO_MEMORY + 8], 0x2F332F33 
+    mov dword [VIDEO_MEMORY + 8], 0x2F332F33
 
-    call kmain 
+    call kmain
 
     cli
 .halt:
@@ -73,8 +74,3 @@ writep:
 
     pop ebp
     ret
-
-section .bss
-    align 16
-    stack_bot: resb 0x10000
-    stack_top:
