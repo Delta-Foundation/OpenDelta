9c8ed8c3751e4d9fc38eb11056ccff96f15a45ef
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-17 17:22:18 +0500

    Update gdt.c

--- kernel/arch/gdt/gdt.c
+++ kernel/arch/gdt/gdt.c
@@ -1,22 +1,13 @@
 #include "gdt.h"
 
 GDTEntry g_gdt[] = {
-    GDT_ENTRY(0, 0, 0, 0),
-
-    GDT_ENTRY(0,
-              0xFFFFF,
-              GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_SEGMENT | GDT_ACCESS_CODE_READABLE,
-              GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
-
-    GDT_ENTRY(0,
-              0xFFFFF,
-              GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_DATA_SEGMENT | GDT_ACCESS_DATA_WRITEABLE,
-              GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
-
+    {0, 0, 0, 0, 0, 0}, // Null entry
+    {0xFFFF, 0, 0, 0x9A, 0xCF, 0}, // Code
+    {0xFFFF, 0, 0, 0x92, 0xCF, 0}  // Data
 };
 
 GDTDescriptor g_gdt_descriptor = { sizeof(g_gdt) - 1, g_gdt };
 
 void i386_GDT_init(void) {
-    i386_GDT_load(&g_gdt_descriptor, i386_GDT_CODE_SEG, i386_GDT_DATA_SEG);
+    i386_GDT_load(&g_gdt_descriptor, (uint32_t)i386_GDT_CODE_SEG, (uint32_t)i386_GDT_DATA_SEG);
 }
