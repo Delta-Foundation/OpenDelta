266edfd481f26ea6d719ec168e715012bf625b60
deltadev-rsc <deltaqxq174@mail.ru>
2026-07-05 23:19:22 +0500

    Update clean.sh

--- scripts/kern/clean.sh
+++ scripts/kern/clean.sh
@@ -4,22 +4,22 @@ cd ~/OpenDelta/kernel/
 
 function clean {
     echo "clean .obj binaries"
-    rm -f obj/kernel.o obj/stdbase.o obj/idt.o obj/mem.o 
-        \ obj/string.o obj/types.o obj/screen.o obj/gdt.o 
-        \ obj/gdtasm.o obj/intsa.o obj/isr.o obj/tty.o 
-        \ obj/ctype.o obj/ports.o obj/entry.o
-        \ obj/shm.o obj/fs.o obj/list.o obj/pipe.o
-        \ obj/idta.o obj/pic.o obj/fpu.o obj/sys.o
-        \ obj/proc.o obj/task.o obj/isr.o obj/tools.o 
-        \ obj/fat.o obj/disk.o obj/mbr.o obj/elf.o 
-        \ obj/min_dltsh.o obj/ints_c.o
+    rm -f obj/kernel.o obj/stdbase.o obj/idt.o obj/mem.o \ 
+        obj/string.o obj/types.o obj/screen.o obj/gdt.o \ 
+        obj/gdtasm.o obj/intsasm.o obj/isr.o obj/tty.o \
+        obj/ctype.o obj/ports.o obj/entry.o \
+        obj/shm.o obj/fs.o obj/list.o obj/pipe.o \
+        obj/idtasm.o obj/pic.o obj/fpu.o obj/sys.o \
+        obj/proc.o obj/task.o obj/speaker.o obj/tools.o  \
+        obj/fat.o obj/disk.o obj/mbr.o obj/elf.o \
+        obj/min_dltsh.o obj/ints.o obj/mouse.o obj/time.o
 
     echo "clean kernel.map"
     rm -f kernel.map
 
     echo "clean .bin and .img binaries"
-    rm -f img/kernel.bin img/kernel.elf 
-        \ img/boot.bin img/open-delta.img
+    rm -f img/kernel.bin img/kernel.elf \ 
+        img/boot.bin img/open-delta.img
 }
 
 clean
