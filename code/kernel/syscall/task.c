b7da46fef5f248019a708455f6d98f4bc3114144
Deltaqxq <deltaqxq174@mail.ru>
2026-07-16 23:05:41 +0500

    Update task.c

--- code/kernel/syscall/task.c
+++ code/kernel/syscall/task.c
@@ -8,10 +8,9 @@ page_dir_t *current_dir;
 
 uintptr_t frozen_stack = 0;
 
-void tackingInstall(void) 
+void taskingInstall(void) 
 {
     IRQ_OFF;
-
     prints("\n[INFO]: initializing multi-tasking\n", WHITE);
 
     initProcTree();
@@ -19,8 +18,13 @@ void tackingInstall(void)
     kernel_idle_task = spawnKIdle();
 
 #if 0
+    set_process_environment((sys_process_t *)current_process, current_dir);
 #endif
-
+    
+    switchPageDir(current_process->theard.page_dir);
+    frozen_stack = (uintptr_t)kmalloc(KERNEL_STACK_SIZE, 0, (uint32_t*)&current_process);
+    IRQ_RES;
+    prints("[OK]: multi-tasking initializing is finished!\n", GREEN);
 }
 
 void taskExit(int retval) {
