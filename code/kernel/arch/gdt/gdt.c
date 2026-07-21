517c86fe1f22f842c1eadd120389d9b2ee8c22ed
Deltaqxq <deltaqxq174@mail.ru>
2026-07-21 22:38:32 +0500

    Update gdt.c

--- code/kernel/arch/gdt/gdt.c
+++ code/kernel/arch/gdt/gdt.c
@@ -3,27 +3,6 @@
 static gdt_entry gdt[3];
 static gdt_ptr gp;
 
-__attribute__((noinline, naked))
-static void gdt_reload_segments(void) {
-    __asm__ __volatile__ (
-        "ljmp $0x08, $1f \n\t"
-        "1: \n\t"
-        "movw $0x10, %ax \n\t"
-        "movw %ax, %ds \n\t"
-        "movw %ax, %es \n\t"
-        "movw %ax, %fs \n\t"
-        "movw %ax, %gs \n\t"
-        "movw %ax, %ss \n\t"
-        "ret \n\t"
-    );
-}
-
-__attribute__((noinline))
-static void gdt_flush(void) {
-    __asm__ __volatile__ ( "lgdt %0" : : "m" (gp) : "memory" );
-    gdt_reload_segments();
-}
-
 __attribute__((noinline))
 void gdt_init(void) {
     /* Null descriptor */
@@ -49,8 +28,8 @@ void gdt_init(void) {
         .base_high = 0x00 
     };
 
-    gp.limit = (uint16_t)(sizeof(gdt) - 1);
+    gp.limit = sizeof(gdt) - 1;
     gp.ptr = (gdt_entry *)gdt;
 
-    gdt_flush();
+    gdt_load(&gp, GDT_DATA_SEG, GDT_CODE_SEG);
 }
