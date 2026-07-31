48b952588506f74de2c3f6ef40741fcda5344c89
Deltaqxq <deltaqxq174@mail.ru>
2026-07-31 23:15:38 +0500

    Update tools.asm

--- code/kernel/tools/fat/asm/tools.asm
+++ code/kernel/tools/fat/asm/tools.asm
@@ -1,6 +1,6 @@
 ;-------------------------------------------|
 ;                                           |
-; pmode_func - функция 32-х битного режима. |
+; lmode_func - функция 64-х битного режима. |
 ; rmode_func - функция 16 битного режима.   |
 ; func - обычная функция.                   |
 ;                                           |
@@ -12,11 +12,11 @@ RM_STACK_TOP equ 0x9000
 
 section .bss
     align 4096
-    pmode_stack_backup resd 1
+    lmode_stack_backup resd 1
     rm_arg_buf         resb 32
 
 section .text
-    [bits 32]
+    [bits 64]
     align 4096
 
     global disk_get_drive_params
@@ -24,133 +24,133 @@ section .text
     global disk_read
     global get_next_block
 
-; ---------- Вспомогательные функции (32-bit) ----------
+; ---------- Вспомогательные функции (64-bit) ----------
 disk_get_drive_params:
-    push ebp
-    mov ebp, esp
+    push rbp
+    mov rbp, rsp
     pushad
     push ds
     push es
 
-    mov eax, [ebp + 8]
-    mov [rm_arg_buf + 0], eax
-    mov eax, [ebp + 12]
-    mov [rm_arg_buf + 4], eax
-    mov eax, [ebp + 16]
-    mov [rm_arg_buf + 8], eax
-    mov eax, [ebp + 20]
-    mov [rm_arg_buf + 12], eax
-    mov eax, [ebp + 24]
-    mov [rm_arg_buf + 16], eax
+    mov rax, [rbp + 8]
+    mov [rm_arg_buf + 0], rax
+    mov rax, [rbp + 12]
+    mov [rm_arg_buf + 4], rax
+    mov rax, [rbp + 16]
+    mov [rm_arg_buf + 8], rax
+    mov rax, [rbp + 20]
+    mov [rm_arg_buf + 12], rax
+    mov rax, [rbp + 24]
+    mov [rm_arg_buf + 16], rax
 
-    mov [pmode_stack_backup], esp
+    mov [lmode_stack_backup], rsp
     jmp near enter_real_mode
 
-pmode_disk_get_drive_params:
-    ENTER_PROTECTED_MODE
-    mov esp, [pmode_stack_backup]
+lmode_disk_get_drive_params:
+    ENTER_LONG_MODE
+    mov rsp, [lmode_stack_backup]
     pop es
     pop ds
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov esp, ebp
-    pop ebp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    mov rsp, rbp
+    pop rbp
     ret
 
 disk_reset:
-    push ebp
-    mov ebp, esp
-    push eax
-    push edx
+    push rbp
+    mov rbp, rsp
+    push rax
+    push rdx
     push ds
     push es
 
-    mov eax, [ebp + 8]
-    mov [rm_arg_buf + 0], eax
-    mov [pmode_stack_backup], esp
+    mov rax, [rbp + 8]
+    mov [rm_arg_buf + 0], rax
+    mov [lmode_stack_backup], rsp
     jmp near enter_real_mode
 
-pmode_disk_reset:
-    ENTER_PROTECTED_MODE
-    mov esp, [pmode_stack_backup]
+lmode_disk_reset:
+    ENTER_LONG_MODE
+    mov rsp, [lmode_stack_backup]
     pop es
     pop ds
-    pop edx
-    pop eax
-    mov esp, ebp
-    pop ebp
+    pop rdx
+    pop rax
+    mov rsp, rbp
+    pop rbp
     ret
 
 disk_read:
-    push ebp
-    mov ebp, esp
+    push rbp
+    mov rbp, rsp
     pushad
     push ds
     push es
 
-    mov eax, [ebp + 8]
-    mov [rm_arg_buf + 0], eax
-    mov eax, [ebp + 12]
-    mov [rm_arg_buf + 4], eax
-    mov eax, [ebp + 16]
-    mov [rm_arg_buf + 8], eax
-    mov eax, [ebp + 20]
-    mov [rm_arg_buf + 12], eax
-    mov eax, [ebp + 24]
-    mov [rm_arg_buf + 16], eax
-    mov eax, [ebp + 28]
-    mov [rm_arg_buf + 20], eax
+    mov rax, [rbp + 8]
+    mov [rm_arg_buf + 0], rax
+    mov rax, [rbp + 12]
+    mov [rm_arg_buf + 4], rax
+    mov rax, [rbp + 16]
+    mov [rm_arg_buf + 8], rax
+    mov rax, [rbp + 20]
+    mov [rm_arg_buf + 12], rax
+    mov rax, [rbp + 24]
+    mov [rm_arg_buf + 16], rax
+    mov rax, [rbp + 28]
+    mov [rm_arg_buf + 20], rax
 
-    mov [pmode_stack_backup], esp
+    mov [lmode_stack_backup], rsp
     jmp near enter_real_mode
 
-pmode_disk_read:
-    ENTER_PROTECTED_MODE
-    mov esp, [pmode_stack_backup]
+lmode_disk_read:
+    ENTER_LONG_MODE
+    mov rsp, [lmode_stack_backup]
     pop es
     pop ds
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov esp, ebp
-    pop ebp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    mov rsp, rbp
+    pop rbp
     ret
 
 get_next_block:
-    push ebp
-    mov ebp, esp
+    push rbp
+    mov rbp, rsp
     pushad
     push ds
     push es
 
-    mov eax, [ebp + 8]
-    mov [rm_arg_buf + 0], eax
-    mov eax, [ebp + 12]
-    mov [rm_arg_buf + 4], eax
+    mov rax, [rbp + 8]
+    mov [rm_arg_buf + 0], rax
+    mov rax, [rbp + 12]
+    mov [rm_arg_buf + 4], rax
 
-    mov [pmode_stack_backup], esp
+    mov [lmode_stack_backup], rsp
     jmp near enter_real_mode
 
-pmode_get_next_block:
-    ENTER_PROTECTED_MODE
-    mov esp, [pmode_stack_backup]
+lmode_get_next_block:
+    ENTER_LONG_MODE
+    mov rsp, [lmode_stack_backup]
     pop es
     pop ds
-    pop edi
-    pop esi
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    mov esp, ebp
-    pop ebp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    mov rsp, rbp
+    pop rbp
     ret
 
 section .rmode
@@ -159,9 +159,9 @@ section .rmode
 
 enter_real_mode:
     cli
-    mov eax, cr0
+    mov rax, cr0
     and al, 0xFE
-    mov cr0, eax
+    mov cr0, rax
     jmp 0x1000:rmode_start
 
 rmode_start:
@@ -172,7 +172,7 @@ rmode_start:
     mov sp, 0x9000
     sti
 
-    mov eax, rm_arg_buf
+    mov rax, rm_arg_buf
     mov bp, ax
 
     jmp short rmode_dispatcher
@@ -185,14 +185,14 @@ rmode_disk_get_drive_params:
     mov ah, 0x08
     stc
     int 0x13
-    mov eax, 1
-    sbb eax, 0
+    mov rax, 1
+    sbb rax, 0
 
-    LINEAR_TO_SEG_OFFSET [bp + 4], es, esi, si
+    LINEAR_TO_SEG_OFFSET [bp + 4], es, rsi, si
     mov [es:si], bl
 
     mov eax, [bp + 8]
-    LINEAR_TO_SEG_OFFSET eax, es, esi, si
+    LINEAR_TO_SEG_OFFSET eax, es, rsi, si
     mov bl, ch
     mov bh, cl
     shr bh, 6
@@ -202,17 +202,17 @@ rmode_disk_get_drive_params:
     xor ch, ch
     and cl, 0x3F
     mov eax, [bp + 12]
-    LINEAR_TO_SEG_OFFSET eax, es, esi, si
+    LINEAR_TO_SEG_OFFSET eax, es, rsi, si
     mov [es:si], cx
 
     mov cl, dh
     inc cx
     mov eax, [bp + 16]
-    LINEAR_TO_SEG_OFFSET eax, es, esi, si
+    LINEAR_TO_SEG_OFFSET eax, es, rsi, si
     mov [es:si], cx
 
     push eax
-    jmp near pmode_disk_get_drive_params
+    jmp near lmode_disk_get_drive_params
 
 rmode_disk_reset:
     mov dl, [bp + 0]
@@ -222,7 +222,7 @@ rmode_disk_reset:
     mov eax, 1
     sbb eax, 0
     push eax
-    jmp near pmode_disk_reset
+    jmp near lmode_disk_reset
 
 rmode_disk_read:
     mov dl, [bp + 0]
@@ -236,42 +236,42 @@ rmode_disk_read:
     mov al, [bp + 16]
 
     mov eax, [bp + 20]
-    LINEAR_TO_SEG_OFFSET eax, es, ebx, bx
+    LINEAR_TO_SEG_OFFSET rax, es, rbx, bx
 
     mov ah, 0x02
     stc
     int 0x13
 
-    mov eax, 1
-    sbb eax, 0
-    push eax
-    jmp near pmode_disk_read
+    mov rax, 1
+    sbb rax, 0
+    push rax
+    jmp near lmode_disk_read
 
 rmode_get_next_block:
-    mov eax, [bp + 0]
-    LINEAR_TO_SEG_OFFSET eax, es, edi, di
+    mov rax, [bp + 0]
+    LINEAR_TO_SEG_OFFSET rax, es, rdi, di
 
-    mov eax, [bp + 4]
-    LINEAR_TO_SEG_OFFSET eax, ds, esi, si
+    mov rax, [bp + 4]
+    LINEAR_TO_SEG_OFFSET rax, ds, rsi, si
 
-    mov ebx, [ds:si]
+    mov rbx, [ds:si]
 
-    mov eax, 0xE820
-    mov edx, 0x534D4150
-    mov ecx, 24
+    mov rax, 0xE820
+    mov rdx, 0x534D4150
+    mov rcx, 24
     int 0x15
 
-    cmp eax, edx
+    cmp rax, rdx
     jne short .e820_fail
 
 .e820_ok:
-    mov eax, ecx
-    mov [ds:si], ebx
+    mov rax, rcx
+    mov [ds:si], rbx
     jmp short .e820_end
 
 .e820_fail:
-    mov eax, -1
+    mov rax, -1
 
 .e820_end:
-    push eax
-    jmp near pmode_get_next_block
+    push rax
+    jmp near lmode_get_next_block
