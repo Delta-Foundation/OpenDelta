2a4e2397d9403c1cacf00bfdc8b8a56212f540e4
Deltaqxq <deltaqxq174@mail.ru>
2026-08-16 00:27:07 +0500

    Update commands.c

--- code/shell/src/commands.c
+++ code/shell/src/commands.c
@@ -3,41 +3,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sys/unistd.h>
-#include "cmdlib/files.h"
-#include "cmdlib/simple_comms.h"
+
+#include "cmdlib/commands.h"
 #include "lib/colors.h"
-
-/* constants */
-#define MAX_LINES 512
-#define MAX_LINE_LENGTH 1024
-#define MAX_ARGUMENT_LENGTH 256
-#define MAX_USER_NAME_LENGTH 128
-#define MAX_OS_TITLE_LANGTH 128
-#define MAX_HOST_NAME_LENGTH 128
-#define MAX_QUANTITY_OF_COMMANDS 256
-
-/* structures */
-typedef struct {
-    const char *user_name[MAX_USER_NAME_LENGTH];
-    const char *os_title[MAX_OS_TITLE_LANGTH];
-    const char *host_name[MAX_HOST_NAME_LENGTH];
-    const char *cpu[128];
-    const char *gpu[128];
-    double disk_memory[32768];
-} UserInfo;
-
-typedef struct {
-    int number_of_versions[16];
-    const char *phoenix[128];
-    const char *signalmann[128];
-    const char *teemagnat[128];
-    const char *hanz_hanz[128];
-    const char *iwgnig[128];
-    const char *neu_delta[128];
-    const char *deltonium[128];
-    const char *open_dlt_sh[128];    
-    const char *v_0_0_12_c[128];
-} Versions;
 
 Versions versions = {
     .number_of_versions = 16,
@@ -49,148 +17,84 @@ Versions versions = {
     .neu_delta = "Neu Delta",
     .deltonium = "Deltonium",
     .open_dlt_sh = "OpenDltSH",
-    .v_0_0_12_c = "v0.0.12-c"
+    .v_0_0_14_d = "v0.0.14-d"
 };
 
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-void your_version()
-{
+void shell_version() {
     Versions ver;
     *ver.number_of_versions = 8;
     const char *installed_ver[128];
-    *installed_ver = "v0.0.12-c\n";
+    
+    *installed_ver = "v0.0.14-d\n";    
     printf(T_CYAN "[installed version dltsh]: %s" T_RESET, *installed_ver);
-    *ver.v_0_0_12_c = "v0.0.12-c\n";
-    printf(T_CYAN "[actualy dltsh version]: %s" T_RESET, *ver.v_0_0_12_c);
+   
+    *ver.v_0_0_14_d = "v0.0.14-d\n";
+    printf(T_CYAN "[actualy dltsh version]: %s" T_RESET, *ver.v_0_0_14_d);
 }
 
-// for editor
-void addFile(char fileName[MFNL])
-{
-    int isCreated = 0;
-    FILE * file = fopen(fileName, "a");
-
-    if (file == NULL) {
-        printf(T_RED "[FAILED TO CREATE FILE!]\n" T_RESET);
-        isCreated = 0;
-    }
-    else {
-        fclose(file);
-        isCreated = 1;
-    }
+void clear_screen() {
+    const char *clear_screen_ansi = "\e[1;1H\e[2J";
+    write(STDOUT_FILENO, clear_screen_ansi, 11);
 }
 
-void editorLogo()
-{
-    printf(T_MAGENTA "████████        ███████     █       █   ██  ████████        ███████\n" T_RESET);
-    printf(T_MAGENTA "██     ██      ██     ██    ██     ██       ██     ██      ██     ██\n" T_RESET);
-    printf(T_MAGENTA "██      ██    ██       ██    ██   ██    ██  ██      ██    ██       ██\n" T_RESET);
-    printf(T_MAGENTA "██       ██  ██         ██    █████     ██  ██       ██  ██         ██\n" T_RESET);
-    printf(T_MAGENTA "██       ██  █████████████   ██   ██    ██  ██       ██  █████████████\n" T_RESET);
-    printf(T_MAGENTA "██      ██    ██            ██     ██   ██  ██      ██    ██\n" T_RESET);
-    printf(T_MAGENTA "█████████      ██████████   █       █   ██  █████████      ██████████\n" T_RESET);
-}
+void calculator() {
+    printf(T_BLUE "[калькулятор]\n" T_RESET);
+    double FRST_NUMBER;
+    double SCND_NUMBER;
+    char SYMBOL;
 
-void editor()
-{
-    char lines[MAX_LINES][MAX_LINE_LENGTH];
-   //char inputLine[MAX_LINE_LENGTH];
-    char filename[100];
-    char mode;
-    int lineCount;
-    FILE * file;
-
-    system("clear");
-    editorLogo();
-    printf(T_BLUE "[🗒 текстовый редактор]\n" T_RESET);
-
-    printf(T_CYAN "[Введите имя файла для редактирования]: " T_RESET);
-    scanf("%s", filename);
-    getchar();
-    addFile(filename);
-
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, T_RED "[err]: [не удалось открыть файл: %s]\n" T_RESET, filename);
-        return;
+    int inputResult = scanf("%lf\n %c\n %lf", &FRST_NUMBER, &SYMBOL, &SCND_NUMBER);
+    if (inputResult != 3) {
+        fprintf(stderr, T_RED "[ERR]: [Неверный ввод]\n" T_RESET);
+        while (getchar() != '\n');
     }
 
-    while (fgets(lines[lineCount], sizeof(lines[lineCount]), file) != NULL && lineCount < MAX_LINES) {
-        lineCount++;
-    }
-    fclose(file);
+    double RES;
 
-    printf(T_CYAN "[нажмите 'w' для ввода текста, 'r' для чтения, 'q' для выхода]: \n" T_RESET);
-    while ((mode = getchar()) != 'q') {
-        if (mode == 'w') {
-            printf(T_CYAN "[начните ввод (введите 'E' для выхода)]: \n" T_RESET);
-            while (lineCount < MAX_LINES) {
-                printf("%d: ", lineCount + 1);
-                if (fgets(lines[lineCount], sizeof(lines[lineCount]), stdin) == NULL) {
-                    fprintf(stderr, T_RED "[err]: [ошибка чтения строки.]\n" T_RESET);
-                    break;
-                }
-
-                lines[lineCount][strcspn(lines[lineCount], "\n")] = 0;
-
-                if (strlen(lines[lineCount]) == 0 || strcmp(lines[lineCount], "E") == 0) {
-                    break;
-                }
-
-                strcpy(lines[lineCount], lines[lineCount]);
-                lineCount++;
-
-                if (lineCount >= MAX_LINES) {
-                    printf(T_YELLOW "[warn]: [достигнуто максимальное количество строк. Завершите ввод]\n" T_RESET);
-                    break;
-                }
+    switch (SYMBOL) {
+        case '+':
+            RES = FRST_NUMBER + SCND_NUMBER;
+            break;
+        case '-':
+            RES = FRST_NUMBER - SCND_NUMBER;
+            break;
+        case '*':
+            RES = FRST_NUMBER * SCND_NUMBER;
+            break;
+        case '/':
+            if (SCND_NUMBER == 0) {
+                fprintf(stderr, T_RED "[ERR]: [Деление на ноль!]\n" T_RESET);
+                while (getchar() != '\n');
             }
-
-            if (mode == 'r') {
-                displayFile(filename);
-            }
-        }
+            RES = FRST_NUMBER / SCND_NUMBER;
+            break;
+        default:
+            fprintf(stderr, T_RED "[ERR]: [Неверный символ операции!]\n" T_RESET);
+            while (getchar() != '\n');
     }
 
-    file = fopen(filename, "w");
-    if (file == NULL) {
-        fprintf(stderr, T_RED "[err]: [не удалось открыть файл для записи: %s]\n" T_RESET, filename);
-        return;
-    }
-
-    for (int i = 0; i < lineCount; i++) {
-        fprintf(file, "%s\n", lines[i]);
-    }
-    fclose(file);
-
-    printf(T_CYAN "[Редактор завершил работу]\n" T_RESET);
-    system("clear");
-    welcome();
+    printf(T_GREEN "[результат]: " T_RESET);
+    printf("%.2lf \n", RES);
+    printf(T_CYAN "[калькулятор завершил работу]\n" T_RESET);
 }
 
-void print_fetch()
-{
+void print_fetch() {
     UserInfo user = {
   	    .user_name = "User",
   	    .host_name = "Host",
   	    .os_title = "OpenDelta",
-  	    .cpu = "cpu",
+  	    .cpu = "potato-cpu-gigachad",
         .gpu = "ventilator 3000"
     };
-  	
+
     printf(T_CYAN "        ____               User:        %s\n" T_RESET, *user.user_name);
     printf(T_CYAN "       /   /               ---------------------------\n" T_RESET);
     printf(T_CYAN "      /   / /\\             os:          %s\n" T_RESET, *user.os_title);
-    printf(T_CYAN "     /   / /  \\            kernel:      Linux\n" T_RESET);
+    printf(T_CYAN "     /   / /  \\            kernel:      dltkernel\n" T_RESET);
     printf(T_CYAN "    /   /  \\   \\           shell:       dltsh\n" T_RESET);
-    printf(T_CYAN "   /   /    \\   \\          packeges:    fpkg\n" T_RESET);
+    printf(T_CYAN "   /   /    \\   \\          packeges:    n fpmcp\n" T_RESET);
     printf(T_CYAN "  /   /  /\\  \\   \\         terminal:    Terminal\n" T_RESET);
     printf(T_CYAN " /   /  /  \\  \\   \\        host:        %s\n" T_RESET, *user.host_name);
     printf(T_CYAN "/   /  /    \\  \\   \\       cpu:         %s\n" T_RESET, *user.cpu);
     printf(T_CYAN "\\__/  /______\\  \\__/       gpu:         %s\n" T_RESET, *user.gpu);
 }
-
-#endif
-
