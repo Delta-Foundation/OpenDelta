e5698c4001c43ab709dcd67e54dc631a2e93586f
Deltaqxq <deltaqxq174@mail.ru>
2026-08-17 23:13:04 +0500

    Update boot.asm

--- code/kernel/boot/x86_64/boot.asm
+++ code/kernel/boot/x86_64/boot.asm
@@ -1,191 +1,199 @@
+; === SECTOR 1: Boot sector (512 bytes) === ;
 [bits 16]
 [org 0x7C00]
+
+SECOND_STAGE_ADDR equ 0x7E00
+SECOND_STAGE_SECTORS equ 4
 
 PAGE_BASE equ 0x1000       ; page tables at 0x1000 (safe area)
 
 _start:
     cli
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
+    xor ax, ax 
+    mov ds, ax 
+    mov es, ax 
+    mov ss, ax 
     mov sp, 0x7C00
 
-    ; --- clear screen ---
+    mov [boot_drive], dl 
+
     mov ax, 0x0003
     int 0x10
 
-    mov si, str_16_bit_start
+    mov si, str_16_loading
     call print16
 
-    mov si, str_a20
-    call print16
+    mov ah, 0x02
+    mov al, SECOND_STAGE_SECTORS
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, [boot_drive]
+    mov bx, SECOND_STAGE_ADDR
 
-    in  al, 0x92
-    or  al, 2
-    and al, 0xFE
-    out 0x92, al
+    int 0x13
+    jc disk_error
 
-    mov si, str_jump_to_32
-    call print16
+    jmp second_stage_entry
 
-    lgdt [gdt32_ptr]
-    mov eax, cr0
-    or  al, 1
-    mov cr0, eax
-    jmp 0x08:protected_mode_entry
+disk_error:
+    mov si, str_disk_error
+    call print16 
+    jmp $
 
 print16:
     pusha
-.lp:
+.loop:
     lodsb
-    test al, al
-    jz .dn
+    test al, al 
+    jz .done
     mov ah, 0x0E
     int 0x10
-    jmp .lp
-.dn:
-    popa
-    ret
+    jmp .loop
+.done:
+    popa 
+    ret 
 
-; =============  32-bit Protected Mode  =============
-[bits 32]
-protected_mode_entry:
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov esp, 0x90000
+; --- data of boot sector --- ;
+boot_drive:     db 0
+str_16_loading: db "[DBL 16]: Loading second stage of boot...", 13, 10, 0
+str_disk_error: db "[DBL ERROR]: Disk read failed!!!", 13, 10, 0
 
-    mov edi, 0xB8000 + 3*160
-    mov esi, str_32_bit_start
-    mov ah, 0x0B            ; light cyan
-    call print32
+; --- boot signature --- ;
+times 510 - ($ - $$) db 0
+dw 0xAA55
 
-    mov edi, 0xB8000 + 4*160
-    mov esi, str_cpuid
-    mov ah, 0x0B
-    call print32
+; === SECTOR 2+: second stage of boot === ;
+second_stage_entry:
+    mov si, str_16_bit_start
+    call print16 
 
-    ; --- check CPUID ---
+    ; --- Enable A20 --- ;
+    mov si, str_a20
+    call print16 
+
+    in al, 0x92
+    or al, 2 
+    and al, 0xFE
+    out 0x92, al 
+
+    ; --- Check CPUID --- ;
     pushfd
     pop eax
-    mov ecx, eax
+    mov ecx, eax 
     xor eax, 1 << 21
-    push eax
-    popfd
-    pushfd
-    pop eax
-    push ecx
-    popfd
-    cmp eax, ecx
-    je halt32
+    push eax 
+    popfd 
+    
+    pushfd 
+    pop eax 
+    push ecx 
+    popfd 
+    cmp eax, ecx 
+    je .no_long_mode_16
 
-    ; --- check long mode ---
+    ; --- Check long mode support --- ;
     mov eax, 0x80000001
-    cpuid
-    test edx, 1 << 29
-    jz halt32
+    cpuid 
+    test edx, 1 << 29 
+    jz .no_long_mode_16
 
-    mov edi, 0xB8000 + 5*160
-    mov esi, str_pages_ready
-    mov ah, 0x0B
-    call print32
+    mov si, str_cpuid_ok
+    call print16
 
-    ; --- setup page tables at PAGE_BASE ---
-    mov edi, PAGE_BASE
-    xor eax, eax
-    mov ecx, 4096
-    rep stosd
+    ; --- Setup page tables at PAGE_BASE --- ;
+    mov si, str_setup_pages
+    call print16
 
-    ; PML4[0] -> PDPT
-    mov dword [PAGE_BASE],       PAGE_BASE + 0x1003
-    ; PDPT[0] -> PD
-    mov dword [PAGE_BASE+0x1000], PAGE_BASE + 0x2003
-    ; PD[0..3] -> 2MB pages identity mapped
-    mov dword [PAGE_BASE+0x2000], 0x000083
-    mov dword [PAGE_BASE+0x2008], 0x200083
-    mov dword [PAGE_BASE+0x2010], 0x400083
-    mov dword [PAGE_BASE+0x2018], 0x600083
+    ; Clear 16KB for page tables
+    mov ax, PAGE_BASE >> 4
+    mov es, ax 
+    xor di, di 
+    xor ax, ax 
+    mov cx, 0x2000
+    rep stosw
+    xor ax, ax 
+    mov es, ax 
 
-    mov edi, 0xB8000 + 6*160
-    mov esi, str_jump_to_64
-    mov ah, 0x0B
-    call print32
+    mov dword [PAGE_BASE], PAGE_BASE + 0x1003
+    mov dword [PAGE_BASE + 4], 0
 
-    mov eax, cr4
-    or  eax, 1 << 5
-    mov cr4, eax
+    mov dword [PAGE_BASE + 0x1000], PAGE_BASE + 0x2003
+    mov dword [PAGE_BASE + 0x1004], 0
 
+    mov dword [PAGE_BASE + 0x2000], 0x000083
+    mov dword [PAGE_BASE + 0x2008], 0x200083
+    mov dword [PAGE_BASE + 0x2010], 0x400083
+    mov dword [PAGE_BASE + 0x2018], 0x600083
+
+    mov si, str_jump_to_64
+    call print16
+
+    ; --- enter long mode directly from real mode --- ;
+    cli 
+
+    ; Load GDT
+    lgdt [gdt_ptr]
+
+    ; Enable PAE in CR4
+    mov eax, cr4 
+    or eax, 1 << 5
+    mov cr4, eax 
+
+    ; Set CR3 to page table base
     mov eax, PAGE_BASE
-    mov cr3, eax
+    mov cr3, eax 
 
+    ; Enable long mode in EFER MSR
     mov ecx, 0xC0000080
     rdmsr
-    or  eax, 1 << 8
+    or eax, 1 << 8
     wrmsr
 
+    ; Enable paging + protected mode
     mov eax, cr0
-    or  eax, 1 << 31
-    mov cr0, eax
+    or eax, (1 << 31) | (1 << 0)
+    mov cr0, eax 
 
-    lgdt [gdt64_ptr]
+    ; Far jump to 64-bit mode
     jmp 0x08:long_mode_entry 
 
-halt32:
-    mov edi, 0xB8000 + 5 * 160
-    mov esi, str_err 
-    mov ah, 0x0C
-    call print32
+.no_long_mode_16:
+    mov si, str_error_long_mode
+    call print16 
     jmp $
 
-print32:
-    push edi
-    push esi
-.lp:
-    lodsb
-    test al, al
-    jz .dn
-    stosw
-    jmp .lp
-.dn:
-    pop esi
-    pop edi
-    ret
-
-; =============  64-bit Long Mode  =============
+; === 64-bit long mode === ;
 [bits 64]
 long_mode_entry:
     mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
+    mov ds, ax 
+    mov es, ax 
+    mov ss, ax 
     mov rsp, 0x90000
 
     mov edi, 0xB8000 + 7 * 160
     mov rsi, str_64_bit_start
-    mov ah, 0x0D            ; light magenta
+    mov ah, 0x0D
     call print64
 
     mov edi, 0xB8000 + 8 * 160
     mov rsi, str_64_bit_done
-    mov ah, 0x0A            ; light green
+    mov ah, 0x0A
     call print64
 
-    cli
-    hlt
-    jmp $-2
+    jmp $
 
 print64:
     push rdi
     push rsi
-.lp:
+.loop:
     lodsb
     test al, al
-    jz .dn
+    jz .done
     stosw
-    jmp .lp
-.dn:
+    jmp .loop
+.done:
     pop rsi
     pop rdi
     ret
@@ -193,26 +201,13 @@ print64:
 ; ===============  DATA  ===============
 str_16_bit_start: db "[DBL 16]: Real mode OK", 13, 10, 0
 str_a20:          db "[DBL 16]: A20 enabled", 13, 10, 0
-str_jump_to_32:   db "[DBL 16]: -> Protected mode...", 13, 10, 0
-
-str_32_bit_start: db "[DBL 32]: Protected mode OK", 0
-str_cpuid:        db "[DBL 32]: CPUID: Long Mode OK", 0
-str_pages_ready:  db "[DBL 32]: Page tables ready", 0
-str_jump_to_64:   db "[DBL 32]: -> Long mode...", 0
-str_err:          db "[ERR!!]: No Long Mode!", 0
+str_cpuid_ok:     db "[DBL 16]: CPUID: Long Mode OK", 13, 10, 0
+str_setup_pages:     db "[DBL 16]: Page tables ready", 13, 10, 0
+str_jump_to_64:      db "[DBL 16]: -> Long mode...", 13, 10, 0
+str_error_long_mode: db "[DBL ERROR]: No Long Mode!", 13, 10, 0
 
 str_64_bit_start: db "[DBL 64]: Long mode OK", 0
 str_64_bit_done:  db "[DBL 64]: All transitions done!", 0
-
-; ============  GDT 32-bit  ============
-align 8
-gdt32:
-    dq 0                        ; null
-    dq 0x00CF9A000000FFFF       ; 32-bit code
-    dq 0x00CF92000000FFFF       ; 32-bit data
-gdt32_ptr:
-    dw $ - gdt32 - 1
-    dd gdt32
 
 ; ============  GDT 64-bit  ============
 align 8
@@ -220,10 +215,9 @@ gdt64:
     dq 0                        ; null
     dq 0x00AF9A000000FFFF       ; 64-bit code
     dq 0x00CF92000000FFFF       ; 64-bit data
-gdt64_ptr:
+gdt_ptr:
     dw $ - gdt64 - 1
     dd gdt64
 
-; ============  Boot signature  ============
-times 510 - ($ - $$) db 0
-dw 0xAA55
+; pad stage2 to full sectors
+times ((1 + SECOND_STAGE_SECTORS) * 512) - ($ - $$) db 0
