5a060ccad37ec3728b0896c86a4490595206d38e
Deltaqxq <deltaqxq174@mail.ru>
2026-08-24 22:03:34 +0500

    Update build-shell.sh

--- scripts/shell/build-shell.sh
+++ scripts/shell/build-shell.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
 
+C_SOURCES=(
+    src/term.c
+    src/commands.c
+    src/files.c
+    src/dltsh.c
+    src/editor.c
+    src/simple_comms.c
+)
+
+if [ ! -d "~/OpenDelta/code/shell/bin" ]; then
+    mkdir -p ~/OpenDelta/code/shell/bin
+fi
+
 function build {
-    cd ~/open-delta/kernel/shell/
-    clang src/term.c src/files.c src/dltsh.c src/commands.c src/simple_comms.c -o bin/dltsh
-    clang++ src/help_table.cpp -o bin/table -lncursesw
-	rustc src/dexide.rs -o bin/dexide
-    rustc src/calc.rs -o bin/calc
-    rustc src/clocks.rs -o bin/clocks
+    cd ~/OpenDelta/code/shell/
+    clang "${C_SOURCES[@]}" -o ./bin/dltsh
+    clang++ src/help_table.cpp -o ./bin/table -lncursesw
+    rustc src/dexide.rs -o ./bin/dexide
+    rustc src/calc.rs -o ./bin/calc
+    rustc src/clocks.rs -o ./bin/clocks
 }
 
 function run {
-    ./bin/term
+    ./bin/dltsh
 }
 
 function clean {
-    echo "removing binaries"
-    rm -f bin/dltsh 
-        \ bin/dexide bin/calc 
-        \ bin/clocks bin/table
+    rm -f ./bin/dltsh
+    rm -f ./bin/table
+    rm -f ./bin/dexide
+    rm -f ./bin/calc
+    rm -f ./bin/clocks
 }
 
 build && run
