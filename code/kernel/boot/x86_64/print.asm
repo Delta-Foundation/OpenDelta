8cc0503cd57573dee54e351b60bb107eebcd6290
Deltaqxq <deltaqxq174@mail.ru>
2026-09-01 16:21:29 +0000

    Update print.asm

--- code/kernel/boot/x86_64/print.asm
+++ code/kernel/boot/x86_64/print.asm
@@ -1,43 +1,46 @@
 ; --- Print in 16-bit Real Mode --- ;
+[bits 16]
 print_rmode:
     pusha
-.loop:
+.loop_rmode:
     lodsb
     test al, al 
-    jz .done
+    jz .done_rmode
     mov ah, 0x0E
     int 0x10
-    jmp .loop
-.done:
+    jmp .loop_rmode 
+.done_rmode:
     popa 
     ret 
 
 ; --- Print in 32-bit Protected Mode --- ;
+[bits 32]
 print_pmode:
     push edi 
     push esi 
-.loop:
+.loop_pmode:
     lodsb 
     test al, al 
-    jz .done 
+    jz .done_pmode 
     stosw 
-    jmp .loop 
-.done:
+    jmp .loop_pmode
+.done_pmode:
     pop esi 
     pop edi 
     ret
 
 ; --- Print in 64-bit Long Mode --- ;
+[bits 64]
 print_lmode:
     push rdi
     push rsi
-.loop:
+.loop_lmode:
     lodsb
     test al, al
-    jz .done
+    jz .done_lmode
     stosw
-    jmp .loop
-.done:
+    jmp .loop_lmode 
+.done_lmode:
     pop rsi
     pop rdi
     ret
